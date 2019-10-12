5590167419f993910a84ba83afddd1a866ce8106
Chris Scott <christo9090@gmail.com>
2019-10-12 15:59:22 -0600

    Finished calc and printed total to DOM.

--- inspectionPrice.js
+++ inspectionPrice.js
@@ -1,8 +1,12 @@
-// Global var
-let inspectionPrice = 0
 
 const returnPrice = () => {
+  let inspectionPrice = 0
   let $sqft = $('#HomeSquareFootage')[0].value;
+  let $condo = $('#condoCheck')
+  let $newConst = $('#newConst')
+  let discountPrice = 0;
+
+  // Find base inspection price.
   if ($sqft <= 1000 ) {
     inspectionPrice = 340
   } else if ($sqft > 1000 && $sqft <=1500) {
@@ -32,9 +36,23 @@ const returnPrice = () => {
   } else if ($sqft > 9000) {
     inspectionPrice = 750
   } else {
-
+    alert('Please enter a number.')
   };
-  console.log(inspectionPrice)
+
+  // Find discount
+  if ($condo[0].checked === true && $newConst[0].checked === true) {
+    discountPrice = 25;
+  } else if ($condo[0].checked) {
+    discountPrice = 25;
+  } else if ($newConst[0].checked) {
+    discountPrice = 15;
+  };
+  //find total
+  let finalPrice = inspectionPrice - discountPrice
+  console.log(finalPrice)
+
+  //Print the total to the DOM.
+  $('#pricingReturn').html(`<p>The base price for your inspection is $${finalPrice}.</p>`)
 };
 
 $(document).ready(() => {
